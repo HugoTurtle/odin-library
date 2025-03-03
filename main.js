efa2fe79c6be31737f924858e0e3ea793c430d48
Hugo Perez <hugopereztm12309@gmail.com>
2025-03-03 18:51:41 -0500

    Add displayBook method to render library books

--- main.js
+++ main.js
@@ -18,3 +18,26 @@ function createBook(title, author, pageNum, hasRead) {
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+
+Book.prototype.displayBook = function () {
+    const book = document.createElement('div');
+    book.classList.add('book');
+
+    const author = document.createElement('h3');
+    author.classList.add('author');
+
+    const title = document.createElement('p')
+    title.classList.add('title')
+
+    const pageNum = document.createElement('p');
+    pageNum.classList.add('pages')
+
+    const hasRead = document.createElement('p')
+    hasRead.classList.add('read-status');
+    
+    book.append(author,title,pageNum,hasRead);
+
+    const bookContainer = document.getElementsByClassName('book-container');
+
+    bookContainer.appendChild(book);
+}
