72fc0f41acf1bf6dcc4c5cd256adcf8d8068a173
Hugo Perez <hugopereztm12309@gmail.com>
2025-03-13 18:00:56 -0400

    Remove unused method

--- main.js
+++ main.js
@@ -5,10 +5,6 @@ function Book (title, author, pageNum, hasRead) {
     this.author = author;
     this.pageNum = pageNum;
     this.hasRead = hasRead;
-
-    this.info = function () {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead ? "read" : "not read yet"}`;
-    };
 }
 
 function createBook(title, author, pageNum, hasRead) {
